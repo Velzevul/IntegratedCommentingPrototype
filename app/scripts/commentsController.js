b8eaf1c04ef83202de7543c9727db1a3b7b1cbef
SuneetD <suneetsd@gmail.com>
2016-02-19 15:22:23 -0600

    Update commentsController.js

--- app/scripts/commentsController.js
+++ app/scripts/commentsController.js
@@ -3,72 +3,37 @@ angular.module('comments')
     'use strict';
 
     $scope.activeTab = null;
-
+    HeatmapService.initilize(79);
+    $scope.activeLine = -5;
+    $scope.activeComments = [];
+    $scope.showFilterOptions = false;
+    
     // contextual comments
     $scope.contextualComments = ContextualCommentsService.getAll();
 
+    //simple object for lines
+    $scope.setDivs = function(amount){
+    	return new Array(amount);
+    };
     
-    ContextualCommentsService.setDates();
-
-    //heatmap testing
-
-    //HeatmapService.initilizeHeatmap();
-
-    $scope.$watch('contextualComments', function(){
-
-    });
-
-    //********************************************************
-    //
-    // SLIDER FUNCTIONALITY
-    //
-    //********************************************************
-    
-    //starting pos of sliders
-    $scope.sliderMinMax = {
-      min: 0,
-      max: 50
-    };
-
-    //total slider value
-    $scope.sliderValues = {
-      min: 0,
-      max: 50
-    };
-
-    $scope.hour = 60 * 60 * 1000;
-
-    //sliders filter function ( test implementation )
-    /*$scope.sliderFilter = function(comment){
-      if( n >= $scope.sliderMinMax.min && n <= $scope.sliderMinMax.max){
-        return true;
-      } else if( $scope.sliderValues.max == $scope.sliderMinMax.max){
-        return true;
-      } else{
-        return false;
-      }
-    };*/
-
-    $scope.sliderFilter = function(comment){
-      var dateNow = new Date();
-      var dateComment = new Date(comment.postedOn);
-
-      if( dateComment.getTime() <= ( dateNow.getTime() - $scope.sliderMinMax.min * $scope.hour ) && dateComment.getTime() >= ( dateNow.getTime() - $scope.sliderMinMax.max * $scope.hour )){
-        return true;
-      } else{
-        return false;
-      }
-    };
-
-    //for filtering when max time has changed
-    $scope.$watchGroup(['sliderMinMax.max', 'sliderMinMax.min'], function(){
-        //$scope.filter($scope.sliderMinMax.max);
-        if( $scope.sliderValues.max == $scope.sliderMinMax.max && $scope.sliderValues.min == $scope.sliderMinMax.min ){
-
-        } else {
-
+    $scope.filterLine = function(comment){
+        var i = 0;
+        var temp ;
+        var children = $("[comment-active]");
+        
+        while(children.length > i){
+            temp = children[i];
+            //console.log($scope.activeLine == Math.ceil((angular.element(temp).prop('offsetTop') - 172) / 24))
+            if( temp.id == comment.id ){
+                return true
+            } else {
+                i++;
+            }
         }
+    }
 
-    });
+    $scope.updateActiveComments = function(){
+        //var line = Math.ceil((angular.element(temp).prop('offsetTop') - 172) / 24)
+    }
 
   });
