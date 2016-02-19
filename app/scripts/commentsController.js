371fa8924cda2d5ca2b52080a6570680d94fbac7
SuneetD <suneetsd@gmail.com>
2016-02-19 10:50:44 -0600

    Update commentsController.js

--- app/scripts/commentsController.js
+++ app/scripts/commentsController.js
@@ -1,5 +1,5 @@
 angular.module('comments')
-  .controller('commentsController', function($scope, ContextualCommentsService, SelectionService, GeneralCommentsService) {
+  .controller('commentsController', function($scope, ContextualCommentsService, SelectionService, HeatmapService) {
     'use strict';
 
     $scope.activeTab = null;
@@ -7,7 +7,68 @@ angular.module('comments')
     // contextual comments
     $scope.contextualComments = ContextualCommentsService.getAll();
 
-    // general comments
-    $scope.generalComments = GeneralCommentsService.getAll();
-    $scope.generalCommentFormShown = false;
+    
+    ContextualCommentsService.setDates();
+
+    //heatmap testing
+
+    //HeatmapService.initilizeHeatmap();
+
+    $scope.$watch('contextualComments', function(){
+
+    });
+
+    //********************************************************
+    //
+    // SLIDER FUNCTIONALITY
+    //
+    //********************************************************
+    
+    //starting pos of sliders
+    $scope.sliderMinMax = {
+      min: 0,
+      max: 50
+    };
+
+    //total slider value
+    $scope.sliderValues = {
+      min: 0,
+      max: 50
+    };
+
+    $scope.hour = 60 * 60 * 1000;
+
+    //sliders filter function ( test implementation )
+    /*$scope.sliderFilter = function(comment){
+      if( n >= $scope.sliderMinMax.min && n <= $scope.sliderMinMax.max){
+        return true;
+      } else if( $scope.sliderValues.max == $scope.sliderMinMax.max){
+        return true;
+      } else{
+        return false;
+      }
+    };*/
+
+    $scope.sliderFilter = function(comment){
+      var dateNow = new Date();
+      var dateComment = new Date(comment.postedOn);
+
+      if( dateComment.getTime() <= ( dateNow.getTime() - $scope.sliderMinMax.min * $scope.hour ) && dateComment.getTime() >= ( dateNow.getTime() - $scope.sliderMinMax.max * $scope.hour )){
+        return true;
+      } else{
+        return false;
+      }
+    };
+
+    //for filtering when max time has changed
+    $scope.$watchGroup(['sliderMinMax.max', 'sliderMinMax.min'], function(){
+        //$scope.filter($scope.sliderMinMax.max);
+        if( $scope.sliderValues.max == $scope.sliderMinMax.max && $scope.sliderValues.min == $scope.sliderMinMax.min ){
+
+        } else {
+
+        }
+
+    });
+
   });
