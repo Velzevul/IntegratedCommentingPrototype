8b3b748fd309be0a0285078a3e5be670f22d94e8
SuneetD <suneetsd@gmail.com>
2016-03-03 15:44:52 -0600

    Update commentsController.js

--- app/scripts/commentsController.js
+++ app/scripts/commentsController.js
@@ -1,13 +1,51 @@
 angular.module('comments')
-  .controller('commentsController', function($scope, ContextualCommentsService, SelectionService, GeneralCommentsService) {
+  .controller('commentsController', function($scope, ContextualCommentsService, SelectionService, HeatmapService, InitialCommentsService) {
     'use strict';
 
-    $scope.activeTab = null;
+    $scope.activeTab = 'contextual';
+    HeatmapService.initilize(79);
+    $scope.activeLine = -5;
+    $scope.activeComments = [];
+    $scope.showFilterOptions = false;
+    $scope.relevantAnchors = new Array (79);
+    $scope.optionsPanel = false;
 
+    
     // contextual comments
     $scope.contextualComments = ContextualCommentsService.getAll();
 
-    // general comments
-    $scope.generalComments = GeneralCommentsService.getAll();
-    $scope.generalCommentFormShown = false;
+    //simple object for lines
+    $scope.setDivs = function(amount){
+    	return new Array(amount);
+    };
+    
+    $scope.filterLine = function(comment){
+        var i = 0;
+        var temp ;
+        var children = $("[comment-active]");
+        
+        while(children.length > i){
+            temp = children[i];
+            //console.log($scope.activeLine == Math.ceil((angular.element(temp).prop('offsetTop') - 172) / 24))
+            if( temp.id == comment.id ){
+                return true
+            } else {
+                i++;
+            }
+        }
+    }
+
+    angular.element(document).ready(function(){
+        HeatmapService.initilizeDivs();
+    });
+
+    $scope.setActiveLine = function(number){
+        if(number != $scope.activeLine){
+            $scope.activeLine = number;
+        } else {
+            $scope.activeLine = -5;
+        }
+    }
+
+    InitialCommentsService.addRandomComments();
   });
