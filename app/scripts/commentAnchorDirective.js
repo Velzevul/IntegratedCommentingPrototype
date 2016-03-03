bc94f528ebf41b1d39352953f0570b3ba121d407
SuneetD <suneetsd@gmail.com>
2016-03-03 15:43:42 -0600

    Update commentAnchorDirective.js

--- app/scripts/commentAnchorDirective.js
+++ app/scripts/commentAnchorDirective.js
@@ -10,34 +10,56 @@ angular.module('comments')
       link: function($scope, elem, attrs) {
         $scope.comment = ContextualCommentsService.getOne($scope.id);
 
-        $scope.$parent.$watch('activeTab', function(value) {
-          if (value == 'contextual') {
+        $scope.$parent.$watch('activeLine', function(value) {
+          //console.log(elem.prop('id') ==  1)
+        
+          if( value <= (Math.ceil((angular.element(elem).prop('offsetTop')-172)/24)) +2
+              && value >= (Math.ceil((angular.element(elem).prop('offsetTop')-172)/24)) -2){
             elem.addClass('document__anchor');
-            elem.addClass('document__anchor--' + $scope.comment.color);
+            elem.addClass('document__anchor--' + $scope.comment.color + $scope.comment.score);
+            elem.attr('comment-active', false);
+            if($scope.$parent.activeComments.indexOf(elem.prop('id')) == -1){
+              $scope.$parent.activeComments.push(elem.prop('id'));
+            }
+            if($scope.comment.hasInstructor){
+              elem.addClass('document__anchor--teacher');
+            }
+            console.log()
           } else {
             elem.removeClass('document__anchor');
+            elem.removeClass('document__anchor--' + $scope.comment.color + $scope.comment.score);
             elem.removeClass('document__anchor--' + $scope.comment.color);
+            elem.removeAttr('comment-active', false);
+            if($scope.comment.hasInstructor){
+              elem.removeClass('document__anchor--teacher');
+            }
+            var index = $scope.$parent.activeComments.indexOf(elem.prop('id'));
+            if(index != -1){
+              $scope.$parent.activeComments.splice(index, 1); 
+            }
           }
+
+          ContextualCommentsService.reposition();
         });
 
         $scope.$watch('comment.isSelected', function(value) {
-          if ($scope.$parent.activeTab == 'contextual') {
+          //if ($scope.$parent.activeLine == (Math.ceil((angular.element(elem).prop('offsetTop')-172)/24))) {
             if (value) {
               elem.addClass('document__anchor--selected');
             } else {
               elem.removeClass('document__anchor--selected');
             }
-          }
+          //}
         });
 
         $scope.$watch('comment.highlightedAnchor', function(value) {
-          if ($scope.$parent.activeTab == 'contextual') {
+         // if ($scope.$parent.activeLine == (Math.ceil((angular.element(elem).prop('offsetTop')-172)/24))) {
             if (value) {
               elem.addClass('document__anchor--highlighted');
             } else {
               elem.removeClass('document__anchor--highlighted');
             }
-          }
+          //}
         });
 
         $(elem).click(function() {
