a7edacd81b2ba6b50ef81bee43d544bda170883a
SuneetD <suneetsd@gmail.com>
2016-03-03 15:48:49 -0600

    Update selectAnchorDirective.js

--- app/scripts/selectAnchorDirective.js
+++ app/scripts/selectAnchorDirective.js
@@ -1,20 +1,20 @@
 angular.module('comments')
-  .directive('selectAnchor', function(SelectionService, ContextualCommentsService, $compile) {
+  .directive('selectAnchor', function(SelectionService, HeatmapService, ContextualCommentsService, $compile) {
     'use strict';
 
     return {
       restrict: 'A',
       link: function($scope, elem, attrs) {
         $(elem).mousedown(function(e) {
-          if ($scope.activeTab == 'contextual') {
+          //if ($scope.activeTab == 'contextual') {
             SelectionService.clearSelection();
-          }
+          //}
         });
 
         $(elem).mouseup(function(e) {
-          if ($scope.activeTab == 'contextual') {
+          //if ($scope.activeTab == 'contextual') {
             SelectionService.storeSelection();
-          }
+          //}
 
           if ($scope.selectingContext) {
             ContextualCommentsService.create();
@@ -25,6 +25,9 @@ angular.module('comments')
         $scope.$watch('contextualComments.length', function() {
           $compile(elem.contents())($scope);
           // debugger;
+          //updates heres
+          HeatmapService.initilize(79);
+          
         });
       }
     };
