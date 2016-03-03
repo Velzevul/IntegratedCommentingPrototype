4221d498337bacb0f223a6be053c29021d85cd48
SuneetD <suneetsd@gmail.com>
2016-03-03 15:49:05 -0600

    Update selectionService.js

--- app/scripts/selectionService.js
+++ app/scripts/selectionService.js
@@ -15,6 +15,10 @@ angular.module('comments')
       return  el;
     }
 
+    function setRange(range){
+      this.range = range;
+    }
+
     return {
       clearSelection: function() {
         var newFragment;
@@ -33,13 +37,33 @@ angular.module('comments')
 
         if (window.getSelection().type == 'Range') {
           range = selection.getRangeAt(0);
-          range.surroundContents(noteElement);
+          //allows you to select previous comments
+          noteElement.appendChild(range.extractContents()); 
+          range.insertNode(noteElement);
+          //range.surroundContents(noteElement);
           selection.removeAllRanges();
           selection.addRange(range);
         }
+      },initialRange: function(passedRange){
+        var noteElement = document.createElement('note-temp'),
+        selection = window.getSelection();
+
+        //console.log(selection.type)
+        range = passedRange;
+        noteElement.appendChild(range.extractContents());
+        //console.log(range.extractContents())
+        range.insertNode(noteElement);
+        selection.removeAllRanges();
+        selection.addRange(range);
       },
       hasSelection: function() {
         return range ? true : false;
+      },
+      setRangeInitial: function(rangeToSet, comment){
+        range = rangeToSet;
+        range.surroundContents( makeRealNote(comment) );
+
+        range = null;
       },
       insertRealNote: function(comment) {
         var newFragment;
